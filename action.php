616861116d5cab6797e5996694dcc397311c8ac4
lainme <lainme993@gmail.com>
2014-01-30 22:18:10 +0800

    delete unused lines

--- action.php
+++ action.php
@@ -8,9 +8,6 @@
 
 // must be run within Dokuwik
 if (!defined('DOKU_INC')) die();
-if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC.'lib/plugins/');
-
-require_once DOKU_PLUGIN.'action.php';
 
 class action_plugin_miniblog extends DokuWiki_Action_Plugin {
     public function register(Doku_Event_Handler $controller) {
