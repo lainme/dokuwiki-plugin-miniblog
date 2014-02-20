533466ebddfd1a4074c8aa2c875a30480cb38cde
lainme <lainme993@gmail.com>
2014-02-20 14:46:17 +0800

    style

--- syntax/entry.php
+++ syntax/entry.php
@@ -45,9 +45,9 @@ class syntax_plugin_miniblog_entry extends DokuWiki_Syntax_Plugin {
         $num = 5; // display 5 entries per page
 
         // slice
-        $page = 5*$INPUT->int('page', 0); // index of first entry in current page
-        $less = (($page > 0) ? max(0, $page-$num) : 0)/5; // previous page
-        $more = ((count($entries) > $page+$num) ? $page+$num : 0)/5; // next page
+        $page = $num*$INPUT->int('page', 0); // index of first entry in current page
+        $less = (($page > 0) ? max(0, ($page-$num)/$num) : 0); // previous page
+        $more = ((count($entries) > $page+$num) ? ($page+$num)/$num : 0); // next page
         $entries = array_slice($entries, $page, $num);
 
         // comment count
@@ -59,9 +59,9 @@ class syntax_plugin_miniblog_entry extends DokuWiki_Syntax_Plugin {
         foreach ($entries as $entry) {
             list($head, $content) = plugin_load('helper', 'miniblog_entry')->entry_content($entry['id']);
 
-            $renderer->doc .= '<h1><a href="'.wl($entry['id']).'">'.$head.'</a></h1>';
+            $renderer->doc .= '<h1 class="miniblog_head"><a href="'.wl($entry['id']).'">'.$head.'</a></h1>';
             $renderer->doc .= '<p class="miniblog_info">';
-            $renderer->doc .= dformat($entry['date']).' Â· <a href="'.wl($entry['id'],'',true).'#disqus_thread"/>';
+            $renderer->doc .= dformat($entry['date']).' · '.$entry['user'].' · <a href="'.wl($entry['id'],'',true).'#disqus_thread"></a>';
             $renderer->doc .= '</p>';
             $renderer->doc .= $content;
         }
